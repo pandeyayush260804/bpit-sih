1b5b44a0f1898c25d68f4da0df1a3bac699e46dc
Ayush Pandey <pandeyayush2608@gmail.com>
2026-01-15 16:58:41 +0530

    CORS updation in server.js

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-
 app.use(express.json());
+
 
 /* ---------- ROUTES ---------- */
 app.use("/api/v1", indexRoute);
